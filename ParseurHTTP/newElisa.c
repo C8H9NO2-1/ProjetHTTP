e4be11570af25b1c7444bbda2ac0d41143cf9f5a
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2024-03-27 08:09:57 +0100

    Petites ameliorations dans le fichier newElisa

--- ParseurHTTP/newElisa.c
+++ ParseurHTTP/newElisa.c
@@ -1,3 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <string.h>
+
+#include "structure.h"
+#include "pm.h"
+#include "affichage.h"
+
 int checkCRLF(char requete[], int longueur, int i){
     while ((i+1)<longueur && (requete[i]!=13 || requete[i+1]!=10)){
         i++;
@@ -5,43 +14,44 @@ int checkCRLF(char requete[], int longueur, int i){
     return i;
 }
 
-int compteHeader(char requete[], int i, int longueur, int tabHeader[]){
-    int indice =i;
-    int j=i;
-    int k=0;
-    while (j<longueur){
-        Noeud *test=malloc(sizeof(Noeud));
-        if (checkConnectionHeader(requete, &j, longueur, test)){
+int compteHeader(char requete[], int i, int longueur, int tabHeader[]) {
+    int indice = i;
+    int j = i;
+    int k = 0;
+
+    while (j<longueur) {
+        Noeud *test = malloc(sizeof(Noeud));
+        if (checkConnectionHeader(requete, &j, longueur, test)) {
             free(test);
             tabHeader[k]=1;
             k++;
         }
-        else if(checkContentLengthHeader(requete, &j, longueur, test)){
+        else if(checkContentLengthHeader(requete, &j, longueur, test)) {
             free(test);
             tabHeader[k]=2;
             k++;
         }
-        else if(checkContentTypeHeader(requete, &j, longueur,  test)){
+        else if(checkContentTypeHeader(requete, &j, longueur,  test)) {
             free(test);
             tabHeader[k]=3;
             k++;
         }
-        else if(checkCookieHeader(requete, &j, longueur, test)){
+        else if(checkCookieHeader(requete, &j, longueur, test)) {
             free(test);
             tabHeader[k]=4;
             k++;
         }
-        else if(checkTransferEncodingHeader(requete, &j, longueur, test)){
+        else if(checkTransferEncodingHeader(requete, &j, longueur, test)) {
             free(test);
             tabHeader[k]=5;
             k++;
         }
-        else if(checkExpectHeader(requete, &j, longueur,  test)){
+        else if(checkExpectHeader(requete, &j, longueur,  test)) {
             free(test);
             tabHeader[k]=6;
             k++;
         }
-        else if(checkHostHeader(requete, &j, longueur,  test)){
+        else if(checkHostHeader(requete, &j, longueur,  test)) {
             free(test);
             tabHeader[k]=7;
             k++;
