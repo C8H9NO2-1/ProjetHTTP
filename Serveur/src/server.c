36f185d2011bab0a029d3cd2b81552442d43532b
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2024-05-04 16:44:22 +0200

    Ajout du premier test pour le serveur

--- Serveur/src/server.c
+++ Serveur/src/server.c
@@ -7,6 +7,8 @@
 #include <arpa/inet.h>
 
 #include "header/request.h"
+#include "header/api.h"
+#include "header/checkRessource.h"
 
 #define REPONSE1 "HTTP/1.0 200 OK\r\n"
 #define REPONSE2 "\r\n"
@@ -23,6 +25,43 @@ int main(int argc, char *argv[]) {
         printf("Demande reçue depuis le client %d\n", requete->clientId);
         printf("Client [%d] [%s:%d]\n", requete->clientId, inet_ntoa(requete->clientAddress->sin_addr), htons(requete->clientAddress->sin_port));
         printf("Contenu de la demande %.*s\n\n", requete->len, requete->buf);
+ 
+        printf("===========\n");
+
+        if (parseur(requete->buf, requete->len) != 0) {
+            _Token *r, *r2;
+            void *root;
+
+            root = getRootTree();
+
+            /*On recupere la request-target (il n'y en a qu'une normalement)*/
+            r = searchTree(root, "request-target");
+            int l;
+            char *s;
+            s = getElementValue(r->node, &l);
+            printf("%.*s\n", l, s);
+
+            if (checkPath(s, l)) {
+                printf("C'est OK\n");
+            } else {
+                printf("alert\n");
+            }
+
+            r2 = searchTree(root, "host");
+            int l2;
+            char *s2;
+            s2 = getElementValue(r2->node, &l2);
+            printf("%.*s\n", l2, s2);
+
+            checkExistenceWithHost(s, l, s2, l2);
+
+            purgeElement(&r);
+            purgeElement(&r2);
+            purgeTree(root);
+        } else {
+            printf("Impossible de parser la requête\n");
+        }
+
 
         writeDirectClient(requete->clientId, REPONSE1, strlen(REPONSE1));
         writeDirectClient(requete->clientId, REPONSE2, strlen(REPONSE2));
