ba8949aa0f1a737857d31e523eff4b21f29e49f4
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2024-06-02 23:59:16 +0200

    Première version finale de l'étape 3

--- Serveur/src/server.c
+++ Serveur/src/server.c
@@ -11,6 +11,8 @@
 #include "header/api.h"
 #include "header/elisa.h"
 #include "header/pm.h"
+#include "header/fileLen.h"
+#include "header/tom.h"
 
 #include "header/colours.h"
 
@@ -61,14 +63,16 @@ int main(int argc, char *argv[]) {
             r2 = searchTree(root, "host");
             int lengthHost = 0;
             char *valueHost = NULL;
-            if (r2 != NULL) { //TODO Il faudra bien prendre en compte le fait que le header peut être le nom d'un site ou une adresse ip (dans le cas d'une @ip, il faut aller dans le else)
+            int dns = -1; // On veut savoir si c'est le nom d'un site web ou une @IP
+            bool testHost = semanticHost(root, version, &dns);
+            if (testHost && dns == 0) { //TODO Il faudra bien prendre en compte le fait que le header peut être le nom d'un site ou une adresse ip (dans le cas d'une @ip, il faut aller dans le else)
                 valueHost = getElementValue(r2->node, &lengthHost);
                 if (lengthTarget == 1 && valueTarget[0] == '/') {
                     file = defaultPath(valueHost, lengthHost);
                 } else {
                      file = checkExistenceWithHost(valueTarget, lengthTarget, valueHost, lengthHost);
                 }
-            } else {
+            } else if (testHost) {
                 file = checkExistence(valueTarget, lengthTarget);
             }
             /*printf("%.*s\n", l2, s2);*/
@@ -87,187 +91,272 @@ int main(int argc, char *argv[]) {
             //! On renvoie les potentielles erreurs
             //!====================================
 
+            bool errorSent = false; // Si on a envoyé une erreur, on ne doit pas en envoyer une autre
+
+            red();
+            //? On récupère la méthode pour savoir si on doit envoyer du contexte ou non
+            bool get = false;
+            if (method == GET) {
+                get = true;
+            } else {
+                get = false;
+            }
+            //? On récupère aussi l'état de la connection
+            char *close = NULL;
+            if (connection == KEEPALIVE) {
+                const char *temp = "keep-alive";
+                close = malloc(sizeof(char) * strlen(temp));
+                strcpy(close, temp);
+            } else {
+                const char *temp = "close";
+                close = malloc(sizeof(char) * strlen(temp));
+                strcpy(close, temp);
+            }
+            //? Erreur dans la startline
             if (!testStartLine) {
+                errorSent = true;
                 if (version == -1) {
                     // Dans ce cas, on a une erreur de version
+                    printf("\nErreur 505\n\n");
+                    error(505, 1, close, requete->clientId, get);
+                } else {
+                    // On a une erreur de méthode
+                    printf("\nErreur 501\n\n");
+                    error(501, version, close, requete->clientId, get);
                 }
             }
+
+            //? Erreur dans le champ host
+            if (!errorSent && !testHost) {
+                printf("\nErreur 400\n\n");
+                error(400, version, close, requete->clientId, get);
+                errorSent = true;
+            }
+
+            //? Erreur dans la request-target
+            if (!errorSent && file == NULL) {
+                printf("\nErreur 404\n\n");
+                error(404, version, close, requete->clientId, get);
+            }
+            
+            reset();
 
             purgeElement(&r);
             purgeElement(&r2);
             purgeElement(&r3);
 
+            //? Si on a envoyé une erreur, on ne fait aucune autre vérification
             //! Interface debugging du serveur:
             //!================================
-            printf("\n\n==========\n");
+            if (!errorSent) {
+                printf("\n\n==========\n");
 
-            //? Méthode
-            printf("Méthode demandée par le client: ");
-            green();
-            switch (method) {
-                case GET:
-                    printf("GET\n");
-                    break;
-                case HEAD:
-                    printf("HEAD\n");
-                    break;
-                case POST:
-                    printf("POST\n");
-                    break;
-            }
-            reset();
-
-            //? Ressource demandée
-            if (file != NULL) {
+                //? Méthode
+                printf("Méthode demandée par le client: ");
                 green();
-                printf("La ressource demandée existe sur le serveur\n");
+                switch (method) {
+                    case GET:
+                        printf("GET\n");
+                        break;
+                    case HEAD:
+                        printf("HEAD\n");
+                        break;
+                    case POST:
+                        printf("POST\n");
+                        break;
+                }
                 reset();
-            } else {
-                red();
-                printf("La ressource demandée n'existe pas sur le serveur\n");
-                reset();
-            }
 
-            //? Version HTTP
-            switch (version) {
-                case 0:
-                    printf("La version du client est HTTP/1.0\n");
-                    break;
-                case 1:
-                    printf("La version du client est supérieure ou égale à HTTP/1.1\n");
-                    break;
-                default:
-                    red();
-                    printf("La version du client nous est inconnue\n");
+                //? Ressource demandée
+                if (file != NULL) {
+                    green();
+                    printf("La ressource demandée existe sur le serveur\n");
                     reset();
-                    break;
-            }
-
-            //? Gestion de la connexion
-            purple();
-            switch (connection) {
-                case CLOSE:
-                    printf("Il faut fermer la connexion après la réponse\n");
-                    break;
-                case KEEPALIVE:
-                    printf("Il ne faut pas fermer la connexion après la réponse\n");
-                    break;
-            }
-            reset();
-
-            //? Transfert Encoging
-            listeEncodage *encoding = malloc(sizeof(listeEncodage));
-            encoding->value = NONE;
-            encoding->next = NULL;
-            /*if (semanticTransferCodings(root, encoding, version)) {
-                green();
-                printf("Les valeurs dans le champ Transfer-Encoding sont bien reconnues\n");
-                reset();
-                listeEncodage *temp = encoding;
-                if (temp->value != NONE) {
-                    printf("Il faut:\n");
-                    while (temp != NULL) {
-                        switch (temp->value) {
-                            case CHUNKED:
-                                printf("-> decoder chunk\n");
-                                break;
-                            case GZIP:
-                                printf("-> decoder gzip\n");
-                                break;
-                            case DEFLATE:
-                                printf("-> decoder deflate\n");
-                                break;
-                            case COMPRESS:
-                                printf("-> decoder compress\n");
-                                break;
-                        }
-                        temp = temp->next;
-                    }
-                    purgeListeEncodage(&encoding);
+                } else {
+                    red();
+                    printf("La ressource demandée n'existe pas sur le serveur\n");
+                    reset();
                 }
-            } else {
-                red();
-                printf("Les valeurs du champ Transfer-Encoding ne sont pas reconnues\n");
-                reset();
-            }*/
 
-            //? Accept
-            ContentType ressourceType = typeFromPath(valueTarget, lengthTarget);
-            if (acceptHeaderVerification(root, ressourceType)) {
-                green();
-                printf("Le client accepte le type de notre représentation\n");
-                reset();
-                printf("Celle-ci est: ");
-                blue();
-                switch (ressourceType) {
-                    case HTML:
-                        printf("text/html\n");
+                //? Version HTTP
+                switch (version) {
+                    case 0:
+                        printf("La version du client est HTTP/1.0\n");
                         break;
-                    case CSS:
-                        printf("text/css\n");
-                        break;
-                    case JAVASCRIPT:
-                        printf("text/javascript\n");
-                        break;
-                    case PNG:
-                        printf("image/png\n");
-                        break;
-                    case JPEG:
-                        printf("image/jpeg\n");
-                        break;
-                    case GIF:
-                        printf("image/gif\n");
-                        break;
-                }
-                reset();
-            } else {
-                red();
-                printf("Le client n'accepte pas le type de notre représentation\n");
-                reset();
-            }
-
-            //? Accept-Encoding
-            EncodingState coding;
-            if (acceptEncodingHeaderVerification(root, &coding)) {
-                green();
-                printf("Le client accepte l'encoding de notre représentation (il n'y en a pas)\n");
-                reset();
-            } else {
-                red();
-                printf("Le client n'accepte pas l'encoding de notre représentation\n");
-                reset();
-                printf("Cependant, il accepte l'encoding suivant: ");
-                blue();
-                switch (coding) {
-                    case GZIP:
-                        printf("gzip\n");
-                        break;
-                    case DEFLATE:
-                        printf("deflate\n");
-                        break;
-                    case COMPRESS:
-                        printf("compress\n");
+                    case 1:
+                        printf("La version du client est supérieure ou égale à HTTP/1.1\n");
                         break;
                     default:
                         red();
-                        printf("Erreur lors de la vérification de Accept-Encoding\n");
+                        printf("La version du client nous est inconnue\n");
+                        reset();
+                        break;
+                }
+
+                //? Gestion de la connexion
+                purple();
+                switch (connection) {
+                    case CLOSE:
+                        printf("Il faut fermer la connexion après la réponse\n");
+                        break;
+                    case KEEPALIVE:
+                        printf("Il ne faut pas fermer la connexion après la réponse\n");
                         break;
                 }
                 reset();
-            }
 
+                //? Transfert Encoging
+                listeEncodage *encoding = malloc(sizeof(listeEncodage));
+                encoding->value = NONE;
+                encoding->next = NULL;
+                /*if (semanticTransferCodings(root, encoding, version)) {
+                  green();
+                  printf("Les valeurs dans le champ Transfer-Encoding sont bien reconnues\n");
+                  reset();
+                  listeEncodage *temp = encoding;
+                  if (temp->value != NONE) {
+                  printf("Il faut:\n");
+                  while (temp != NULL) {
+                  switch (temp->value) {
+                  case CHUNKED:
+                  printf("-> decoder chunk\n");
+                  break;
+                  case GZIP:
+                  printf("-> decoder gzip\n");
+                  break;
+                  case DEFLATE:
+                  printf("-> decoder deflate\n");
+                  break;
+                  case COMPRESS:
+                  printf("-> decoder compress\n");
+                  break;
+                  }
+                  temp = temp->next;
+                  }
+                  purgeListeEncodage(&encoding);
+                  }
+                  } else {
+                  red();
+                  printf("Les valeurs du champ Transfer-Encoding ne sont pas reconnues\n");
+                  reset();
+                  }*/
+
+                //? Accept
+                ContentType ressourceType = typeFromPath(valueTarget, lengthTarget);
+                if (acceptHeaderVerification(root, ressourceType)) {
+                    green();
+                    printf("Le client accepte le type de notre représentation\n");
+                    reset();
+                    printf("Celle-ci est: ");
+                    blue();
+                    switch (ressourceType) {
+                        case HTML:
+                            printf("text/html\n");
+                            break;
+                        case CSS:
+                            printf("text/css\n");
+                            break;
+                        case JAVASCRIPT:
+                            printf("text/javascript\n");
+                            break;
+                        case PNG:
+                            printf("image/png\n");
+                            break;
+                        case JPEG:
+                            printf("image/jpeg\n");
+                            break;
+                        case GIF:
+                            printf("image/gif\n");
+                            break;
+                    }
+                    reset();
+                } else {
+                    red();
+                    printf("Le client n'accepte pas le type de notre représentation\n");
+                    reset();
+                }
+
+                //? Accept-Encoding
+                EncodingState coding;
+                if (acceptEncodingHeaderVerification(root, &coding)) {
+                    green();
+                    printf("Le client accepte l'encoding de notre représentation (il n'y en a pas)\n");
+                    reset();
+                } else {
+                    red();
+                    printf("Le client n'accepte pas l'encoding de notre représentation\n");
+                    reset();
+                    printf("Cependant, il accepte l'encoding suivant: ");
+                    blue();
+                    switch (coding) {
+                        case GZIP:
+                            printf("gzip\n");
+                            break;
+                        case DEFLATE:
+                            printf("deflate\n");
+                            break;
+                        case COMPRESS:
+                            printf("compress\n");
+                            break;
+                        default:
+                            red();
+                            printf("Erreur lors de la vérification de Accept-Encoding\n");
+                            break;
+                    }
+                    reset();
+                }
+                //? Si tout c'est bien passé on envoie la réponse au client
+                //? On a beaucoup de choses à vérifier
+                //? La connection est déjà faite au moment des erreurs (dans close)
+                //? On récupère ensuite le type du fichier
+                char type[50];
+                if (ressourceType == HTML) {
+                    const char *temp = "text/html";
+                    strcpy(type, temp);
+                    type[strlen(temp)] = '\0';
+                } else if (ressourceType == CSS) {
+                    const char *temp = "text/css";
+                    strcpy(type, temp);
+                    type[strlen(temp)] = '\0';
+                } else if (ressourceType == JAVASCRIPT) {
+                    const char *temp = "text/javascript";
+                    strcpy(type, temp);
+                    type[strlen(temp)] = '\0';
+                } else if (ressourceType == PNG) {
+                    const char *temp = "text/png";
+                    strcpy(type, temp);
+                    type[strlen(temp)] = '\0';
+                } else if (ressourceType == JPEG) {
+                    const char *temp = "text/jpeg";
+                    strcpy(type, temp);
+                    type[strlen(temp)] = '\0';
+                } else if (ressourceType == GIF) {
+                    const char *temp = "text/gif";
+                    strcpy(type, temp);
+                    type[strlen(temp)] = '\0';
+                }
+
+                long int length = calulContenLen(file);
+                if (method == GET) {
+                    reponse(200, version, type, length, file, close, requete->clientId);
+                } else {
+                    reponse(200, version, type, length, NULL, close, requete->clientId);
+                }
+            }
             //!================================
+            free(close);
             purgeTree(root);
         } else {
             red();
             printf("Impossible de parser la requête\n");
+            error(400, 1, "close", requete->clientId, false);
             reset();
         }
 
 
 
         //error(404, 0, "keep-alive", requete->clientId);
-        reponse(200,0,"text/html",50, "liste.txt" ,"keep-alive", requete->clientId);
+        /*reponse(200,0,"text/html",50, "liste.txt" ,"keep-alive", requete->clientId);*/
 
 
         //writeDirectClient(requete->clientId, REPONSE1, strlen(REPONSE1));
