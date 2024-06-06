913e7daf29e73dc77679e7d18f56ca1b06d47d42
Paracetamol <paracetamol.C8H9NO2@icloud.com>
2024-06-06 14:12:19 +0200

    Revert "Test pour le support des cookies"
    This reverts commit 40bcdcd8c02d63a1293f1908ebf9129529c7e9ec.

--- Serveur/src/header/phpServer.h
+++ Serveur/src/header/phpServer.h
@@ -1,7 +1,7 @@
 #ifndef PHP_S
 #define PHP_S
 
-void phpServerResponse(int test, char *path, int version, char* connection, Method method , unsigned clientid ,char *chaine,
-    char *contentLength, int length1, char *contentType, int length2, char *cookie, int length3);
+void phpServerResponse(char *path, int version, char* connection, Method method , unsigned clientid ,char *chaine, char *contentLength, int length1,
+        char *contentType, int length2, char *cookie, int length3);
 
 #endif
